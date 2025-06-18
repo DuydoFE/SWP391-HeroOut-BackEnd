31477b35e6680cb1beb37a0c56282cac6efe66ab
DuydoFE <manhlon18@gmail.com>
2025-06-19 03:06:36 +0700

    FixApiPostEventParticipant

--- src/main/java/com/demo/demo/api/EventParticipationAPI.java
+++ src/main/java/com/demo/demo/api/EventParticipationAPI.java
@@ -3,6 +3,7 @@ package com.demo.demo.api;
 import com.demo.demo.dto.EventParticipationRequest;
 import com.demo.demo.entity.EventParticipation;
 import com.demo.demo.service.EventParticipationService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,37 +12,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/participations")
+@SecurityRequirement(name = "api")
+
 public class EventParticipationAPI {
 
     @Autowired
-    private EventParticipationService participationService;
+    private EventParticipationService eventParticipationService;
 
     @GetMapping
     public ResponseEntity<List<EventParticipation>> getAll() {
-        return ResponseEntity.ok(participationService.getAllParticipations());
+        return ResponseEntity.ok(eventParticipationService.getAllParticipations());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EventParticipation> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(participationService.getParticipationById(id));
+        return ResponseEntity.ok(eventParticipationService.getParticipationById(id));
     }
 
     @PostMapping
     public ResponseEntity<EventParticipation> create(@RequestBody EventParticipationRequest request) {
-        EventParticipation created = participationService.createParticipation(request);
+        EventParticipation created = eventParticipationService.createParticipation(request);
         return ResponseEntity.ok(created);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EventParticipation> update(@PathVariable Long id,
                                                      @RequestBody EventParticipationRequest request) {
-        EventParticipation updated = participationService.updateParticipation(id, request);
+        EventParticipation updated = eventParticipationService.updateParticipation(id, request);
         return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        participationService.deleteParticipation(id);
+        eventParticipationService.deleteParticipation(id);
         return ResponseEntity.noContent().build();
     }
 }
